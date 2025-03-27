cc8f068765965fb733004028a128b29ef93bc142
jaronlong52 <longjm3@appstate.edu>
2025-03-27 16:35:28 -0400

    started api connection

--- regex-quiz/src/App.tsx
+++ regex-quiz/src/App.tsx
@@ -1,11 +1,28 @@
 import Header from "./components/header";
 import Prompt from "./components/prompt";
+import { useEffect, useState } from "react";
+import api from "./api/api";
 
 export default function App() {
+	const [prompt, setPrompt] = useState("example Prompt");
+
+	const getPrompt = async () => {
+		try {
+			const response = await api.get("/prompt");
+			setPrompt(response.data.prompt);
+		} catch (error) {
+			console.error("Error fetching prompt", error);
+		}
+	};
+
+	// useEffect(() => {
+	// 	getPrompt();
+	// }, []);
+
 	return (
 		<div>
 			<Header></Header>
-			<Prompt promptText="^.*$"></Prompt>
+			<Prompt promptText={prompt}></Prompt>
 		</div>
 	);
 }
