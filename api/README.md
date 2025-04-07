c874d8793b53ff45c2ea326d7318d64c0ce78120
willriddick <willriddick>
2025-04-07 13:42:25 -0400

    Added basic difficulty functionality

--- api/README.md
+++ api/README.md
@@ -2,16 +2,19 @@
 venv\Scripts\activate
 
 # Run development:
-python -m main 
+`python -m main`
+
+# Run prompt builder for testing:
+`python -m prompts.prompt_builder`
 
 # Build docker container:
-docker build -t regex-api .
+`docker build -t regex-api .`
 
 # Run docker container:
-docker run -p 8080:8080 regex-api
+`docker run -p 8080:8080 regex-api`
 
 # Tag image to artifact registry:
-docker tag regex-api us-central1-docker.pkg.dev/regex-quiz-455418/regex-quiz/regex-api
+`docker tag regex-api us-central1-docker.pkg.dev/regex-quiz-455418/regex-quiz/regex-api`
 
 # Push image to artifact registry:
-docker push us-central1-docker.pkg.dev/regex-quiz-455418/regex-quiz/regex-api
+`docker push us-central1-docker.pkg.dev/regex-quiz-455418/regex-quiz/regex-api`
--- api/prompts/prompt_builder.py
+++ api/prompts/prompt_builder.py
@@ -1,5 +1,6 @@
+import random
 from . import Prompt
-from components import Quantifier, CharClass, Anchor, Group
+from components import Component, Quantifier, CharClass, Group, Anchor
 
 
 MIN_DIFFICULTY = 0
@@ -15,20 +16,42 @@ def build_prompt(difficulty=0, num_strings=3) -> Prompt:
     :return: A Prompt object containing the regex pattern and generated strings.
     """
     prompt = Prompt()
-
     prompt.add_component(Anchor('^'))
-    prompt.add_component(CharClass.random())
-    prompt.add_component(Group.random())
-    prompt.add_component(Quantifier.random())
-    prompt.add_component(CharClass.random())
+
+    credit = (difficulty + 1) * 4 
+
+    while credit > 0:
+        credit -= add_component(prompt)
+
     prompt.add_component(Anchor('$'))
 
     prompt.build(num_strings)
     return prompt
 
 
+def add_component(prompt) -> int:
+    """
+    Add a random component to the prompt.
+
+    :param prompt: The Prompt object to add components to.
+    :return: The number of credits used.
+    """
+    component: tuple[Component, int] = random.choice([
+        (CharClass, 3),
+        (Group, 4)
+    ])
+    prompt.add_component(component[0].random())
+    credit = component[1]
+    
+    if random.randint(0, 1):
+        prompt.add_component(Quantifier.random())
+        credit += 2
+
+    return credit
+
+
 def main():
-    prompt = build_prompt()
+    prompt = build_prompt(4)
     print(prompt)
 
 
